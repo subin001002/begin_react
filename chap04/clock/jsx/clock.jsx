6823c904531d65125b7964e7a05619aa27288456
subin001002 <subin001002@gmail.com>
2021-10-20 03:05:52 +0900

    first week

---
+++ chap04/clock/jsx/clock.jsx
@@ -0,0 +1,27 @@
+class Clock extends React.Component {
+    constructor(props) {
+        super(props)
+        this.launchClock()
+        this.state = {currentTime: (new Date()).toLocaleString('en')}
+    }
+    launchClock() {
+        setInterval(() => {
+            console.log('Updating Time...')
+            this.setState({
+                currentTime: (new Date()).toLocaleString('en')
+            })
+        }, 1000)
+    }
+    render() {
+        console.log("Rendoring..")
+        return <div>
+            <AnalogDisplay time={this.state.currentTime}/>
+            <DigitalDisplay time={this.state.currentTime}/>
+        </div>
+    }
+}
+
+ReactDOM.render(
+    <Clock/>,
+    document.getElementById('content')
+)
